279c4624d9c8ae1d2b9102d30cb736383b1e44c8
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-26 12:20:12 -0300

    Deleting movies before importing new ones
    * Full import instead of delta one
    * New logs added

--- backend/src/main/java/br/com/alura/sevendaysofcode/service/MovieService.java
+++ backend/src/main/java/br/com/alura/sevendaysofcode/service/MovieService.java
@@ -2,6 +2,8 @@ package br.com.alura.sevendaysofcode.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.sevendaysofcode.controller.dto.MovieResponseDTO;
@@ -13,6 +15,8 @@ import br.com.alura.sevendaysofcode.repository.MovieRepository;
 
 @Service
 public class MovieService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MovieService.class);
     
     private final ImdbGateway gateway;
     private final MovieImdbMapper movieImdbMapper;
@@ -29,9 +33,17 @@ public class MovieService {
     }
 
     public void fetchAndImport250Movies() {
+        LOG.warn("Deleting all movies");
+        movieRepository.deleteAll();
+        
+        LOG.warn("Fetching top 250 movies from IMDB processor");
         ImdbMovieResponseDTO top250Movies = gateway.getTop250Movies();
+
+        LOG.warn("Persisting top 250 movies retrieved into database");
         List<Movie> movies = top250Movies.items().stream().map(Movie::new).toList();
         movieRepository.saveAll(movies);
+
+        LOG.warn("fetchAndImport250Movies executed successfully!");
     }
 
 }
