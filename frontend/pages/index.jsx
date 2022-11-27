6dcba9d794a0bed8de1957d468947430ca6fba74
Alexandre Oliveira <alexmdo@gmail.com>
2022-11-26 21:06:48 -0300

    Added pagination feature

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,18 +1,21 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Router from 'next/router';
 import { Container, Row, Card, Button, Image } from 'react-bootstrap'
 
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:8080/movies/top250");
-  const allPostsData = await res.json();
+export async function getServerSideProps({ query: { page = 0 } }) {
+  const res = await fetch(`http://localhost:8080/movies/top250?page=${page}`);
+  const data = await res.json();
   return {
     props: {
-      allPostsData
+      data,
+      page: parseInt(page, 10)
     }
   };
 }
 
-export default function Home({allPostsData}) {
-  const movies = allPostsData.movies;
+export default function Home({ data, page }) {
+  const movies = data.content;
   return (
     <Container className="md-container">
       <Head>
@@ -25,21 +28,37 @@ export default function Home({allPostsData}) {
         </h1>
         <Container>
           <Row className="justify-content-md-between">
-            {movies.map(({title, urlImage, rating, year}) => (
+            {movies.map(({ title, urlImage, rating, year }) => (
               <Card key={title} className="sml-card">
-              <Card.Body>
-                <Card.Title>{title}</Card.Title>
                 <Card.Body>
-                  <Image src={urlImage} rounded thumbnail fluid>
-                  </Image> 
+                  <Card.Title>{title}</Card.Title>
+                  <Card.Body>
+                    <Image src={urlImage} rounded thumbnail fluid>
+                    </Image>
+                  </Card.Body>
+                  <Card.Footer>
+                    Nota: {rating} - Ano: {year}
+                  </Card.Footer>
                 </Card.Body>
-                <Card.Footer>
-                  Nota: {rating} - Ano: {year}
-                </Card.Footer>
-              </Card.Body>
-            </Card>
+              </Card>
             ))}
           </Row>
+          <Row className='justify-content-md-center'>
+            <Button
+              onClick={() => Router.push(`/?page=${page - 1}`)}
+              disabled={page <= 0}>
+              Anterior
+            </Button>
+            <p>&nbsp;&nbsp;</p>
+            <Button onClick={() => Router.push(`/?page=${page + 1}`)}>
+              Proximo
+            </Button>
+            <p>&nbsp;&nbsp;</p>
+            <Link href="/?page=0">
+              {` <<`}
+            </Link>
+          </Row>
+
         </Container>
       </Container>
 
